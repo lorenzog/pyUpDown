08e9c331045119ab665e3989b212716f26f33456
lor <lor@cushty>
2017-12-13 08:59:43 +0000

    Serves file with a key (XOR) and/or base64 encoding.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pyUpDown
-Basic HTTP(s) Web Server with upload and download
+Basic HTTP(s) Web Server with upload and download.
+
+This server is designed to:
+
+ 1. Run with no dependencies apart from standard Python libraries.
+ 2. Return every file as text/html.
+ 3. Allow for file upload
+
+Its purpose is to aid in exfiltrating data during a penetration test
+(TODO: use socat to run it over ssl?) and transfer scripts, commandlets
+and 
--- s.py
+++ s.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 import argparse
+import base64
+import io
 import os
 import shutil
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-from http import HTTPStatus
+import sys
+from itertools import cycle
+from tempfile import TemporaryFile
+if sys.version_info.major >= 3:
+    from http.server import SimpleHTTPRequestHandler, HTTPServer
+    from http import HTTPStatus
+    from urllib.parse import urlparse, parse_qs
+else:
+    raise SystemExit("Run via python3")
 
 
-PAGE = b'''
+UPLOAD_FORM = b'''
 <!doctype html>
 <title>Upload new File</title>
 <h1>Upload new File</h1>
@@ -16,14 +25,49 @@ PAGE = b'''
 </form>
 '''
 
+
+def encode(key, what):
+    # inspired by
+    # https://dustri.org/b/elegant-xor-encryption-in-python.html
+    # encoded = b''.join(chr(ord(c) ^ ord(k)) for c, k in zip (what, cycle(key)))
+    encoded_chars = []
+    for c, k in zip(what, cycle(bytearray(key, 'utf-8'))):
+        # both 'c' and 'k' are integers if the file is read with 'b'
+        print("c: {} {}".format(c, type(c)))
+        print("k: {} {}".format(k, type(k)))
+        xor = c ^ k
+        print(chr(xor))
+        encoded_chars.append(chr(xor))
+    print("Encoded: {} {}".format(type(encoded_chars), repr(encoded_chars)))
+    return ''.join(encoded_chars)
+
+
 class Handler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path.endswith('/upload'):
+        # get query and path
+        try:
+            u = urlparse(self.path)
+            query = parse_qs(u.query)
+            # TODO get mime type?
+            key = query.get('key')
+            if key is not None:
+                key = key[0]
+                print("Encoding with: {}".format(key))
+            mime = query.get('mime', 'text/plain')
+            b64 = True if query.get('b64') is not None else False
+            print("Base64: {}".format(b64))
+            path = u.path
+        except ValueError as e:
+            print("Cannot parse URL: {}".format(e))
+            path = self.path
+
+        print("Path: {}".format(path))
+        if path.endswith('/upload'):
             stuff = io.BytesIO()
             stuff.write(UPLOAD_FORM)
             stuff.seek(0)
         else:
-            what = self.translate_path(self.path)
+            what = self.translate_path(path)
 
         stuff = None
         if os.path.isdir(what):
@@ -34,16 +78,44 @@ class Handler(SimpleHTTPRequestHandler):
 
         try:
             with open(what, 'rb') as f:
-                # f = open(path, 'rb')
                 fs = os.fstat(f.fileno())
                 self.send_response(HTTPStatus.OK)
-                self.send_header("Content-Length", str(fs[6]))
                 self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-                # force text/plain
-                self.send_header("Content-type", "text/plain")
-                # or: self.guess_type(path)
-                self.end_headers()
-                shutil.copyfileobj(f, self.wfile)
+                # defaults to text/plain
+                self.send_header("Content-type", mime)
+
+                # encode
+                if key is not None:
+                    # TODO read in chunks and write to self.wfile?
+                    encoded = encode(key, f.read())
+                    encoded_b64 = base64.b64encode(bytearray(encoded, 'utf-8'))
+                    print("b64 {} len: {}".format(encoded_b64, len(encoded_b64)))
+                    tmp = TemporaryFile()
+                    tmp.write(encoded_b64)
+                    self.send_header("Content-Length", str(len(encoded_b64)))
+                    self.end_headers()
+                    tmp.seek(0)
+                    shutil.copyfileobj(tmp, self.wfile)
+                    tmp.close()
+                    return
+
+                if b64 is True:
+                    b64 = base64.b64encode(f.read())
+                    tmp = TemporaryFile()
+                    tmp.write(b64)
+                    self.send_header("Content-Length", str(len(b64)))
+                    print("b64 {} len: {}".format(b64, len(b64)))
+                    self.end_headers()
+                    tmp.seek(0)
+                    shutil.copyfileobj(tmp, self.wfile)
+                    tmp.close()
+                    return
+
+                else:
+                    self.send_header("Content-Length", str(fs[6]))
+                    self.end_headers()
+                    shutil.copyfileobj(f, self.wfile)
+                    return
 
         except OSError:
             self.send_error(HTTPStatus.NOT_FOUND, "File not found")
@@ -60,7 +132,7 @@ class Handler(SimpleHTTPRequestHandler):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('port', type=int)
+    parser.add_argument('-p', '--port', type=int, default=8080)
     parser.add_argument('-b', '--bind-to', default='0.0.0.0')
     args = parser.parse_args()
 
